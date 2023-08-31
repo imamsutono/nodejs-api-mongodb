02baf62604d860e3b4ea98c982ebe01394b281ec
imamsutono <imamsutono@detik.com>
2023-08-31 15:08:19 +0700

    add sub router

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 export const app = express()
+const router = express.Router()
 
 app.disable('x-powered-by')
 
@@ -11,6 +12,12 @@ app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
+
+router.get('/me', (req, res) => {
+    res.send({ me: 'hello' })
+})
+
+app.use('/api', router)
 
 const log = (req, res, next) => {
     console.log('logging')
